4d252e537caea3099b265855819158d1e76f4e5f
ganesh229011 <ganeshdesh827@gmail.com>
2025-07-18 16:24:23 +0530

    initial github setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ function App() {
     <div>
       <h1 className='mt-24 font-bold'>hello Tailwind</h1>
       <p className='text-red-900 mx-48'>browser</p>
+      <h1 className="text-5xl">Life Insurance</h1>
     </div>
   );
 }
